239117efee72896d52317146c2c47128043baa76
night371709392 <371709392@qq.com>
2026-03-18 21:55:58 +0800

    开发页面

--- src/views/ChatRoom/friend.vue
+++ src/views/ChatRoom/friend.vue
@@ -7,6 +7,14 @@
         </div>
         <button><i class="iconfont icon-jiahao"></i></button>
       </div>
+      <div class="top-item">
+        <div class="head-image">
+          <img src="@/assets/img/ChatRoom/newfriend.png" alt="">
+        </div>
+        <div class="right">
+          <p>新的朋友</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -52,18 +60,55 @@ export default {
   border: 1px solid #e4e7ed;
   background-color: #fff;
 }
+.friend .list .header .ipt input:hover {
+  border-color: #2830D3;
+}
+.friend .list .header .ipt input:focus {
+  border-color: #2830D3;
+}
 .friend .list .header button {
   width: 32px;
   height: 32px;
-  display: inline-block;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   margin: 5px;
   border-radius: 50%;
   border: 1px solid #fff;
   background-color: #F4F4FC;
+  cursor: pointer;
+}
+.friend .list .header button:hover {
+  color: #EAEAFB;
+  transform: scale(1.1);
 }
 .friend .list .header .iconfont {
   color: #3458DA;
   font-size: 16px;
   font-style: normal;
+}
+.friend .list .top-item {
+  height: 60px;
+  display: flex;
+  position: relative;
+  padding: 5px 10px;
+  align-items: center;
+  white-space: nowrap;
+  cursor: pointer;
+}
+.friend .list .top-item .head-image {
+  width: 45px;
+  height: 45px;
+}
+.friend .list .top-item .head-image img {
+  width: 45px;
+  height: 45px;
+  border-radius: 50%;
+}
+.friend .list .top-item .right {
+  padding-left: 10px;
+  text-align: left;
+  flex: 1;
+  font-size: 14px;
 }
 </style>
